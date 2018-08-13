37fb9191b3e4f900f35b25a4e2f54cee608deed2
Robert McComb <mccombenator@gmail.com>
2018-08-13 21:15:21 +1000

    filtering working

--- OdataWebApp/App_Start/WebApiConfig.cs
+++ OdataWebApp/App_Start/WebApiConfig.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
+using OdataWebApp.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -10,15 +13,34 @@ namespace OdataWebApp
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
-            // Web API routes
             config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+            config.AddODataQueryFilter();
+
+            ODataModelBuilder builder = new ODataConventionModelBuilder();
+
+            builder.EntitySet<Employee>("Employees");
+
+            // http://odata.github.io/WebApi/#13-01-modelbound-attribute
+            // the default setting for WebAPI OData is : 
+            // client can’t apply $count, $orderby, $select, $top, $expand, $filter in the query, 
+            // query like localhost\odata\Customers ?$orderby = Name will failed as BadRequest, 
+            // because all properties are not sortable by default, 
+            // this is a breaking change in 6.0.0
+            builder.EntityType<Employee>()
+                .OrderBy().Filter().Count().Select().Expand();
+
+            config.MapODataServiceRoute(
+                routeName: "ODataRoute",
+                routePrefix: null,
+                model: builder.GetEdmModel());
+
+
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional });
+
         }
     }
 }
---
+++ OdataWebApp/Controllers/EmployeesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNet.OData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OdataWebApp.Controllers
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string PostCode { get; set; }
+    }
+
+    // see https://www.asp.net/web-api
+
+    public class EmployeesController : ODataController
+    {
+        List<Employee> _employees = new List<Employee>
+        {
+            new Employee { ID = 1, Name = "Tom", PostCode = "4105" },
+            new Employee { ID = 2, Name = "John", PostCode = "1234" },
+            new Employee { ID = 3, Name = "Bert", PostCode = "4321" },
+            new Employee { ID = 4, Name = "Andrew", PostCode = "9999" },
+            new Employee { ID = 5, Name = "Quentin", PostCode = "7445" },
+            new Employee { ID = 5, Name = "Crispin", PostCode = "0113" },
+        };
+
+        // Enable filtering in WebApiConfig !
+        // GET /Employees?$filter=startswith(Name, 'Bert') eq true
+        [EnableQuery]
+        public IEnumerable<Employee> Get()
+        {
+            return _employees;
+        }
+    }
+}
